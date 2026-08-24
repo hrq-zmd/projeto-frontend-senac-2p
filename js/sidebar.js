f2fe839b426bb3625b1e354b769fa5deab608884
Henrique Zmuda <zmuda.fis@gmail.com>
2026-08-24 14:47:39 -0300

    finish.

--- js/sidebar.js
+++ js/sidebar.js
@@ -2,8 +2,22 @@ const departmentTitles = document.querySelectorAll(".department_title");
 
 departmentTitles.forEach((title) => {
   title.addEventListener("click", () => {
-    const list = title.nextElementSibling;
+    const departmentList = title.nextElementSibling;
 
-    list.classList.toggle("ativo");
+    departmentList.classList.toggle("ativo");
+  });
+});
+
+const departmentSubcategories = document.querySelectorAll(
+  ".department_subcategory",
+);
+
+departmentSubcategories.forEach((subcategory) => {
+  subcategory.addEventListener("click", (event) => {
+    event.preventDefault();
+
+    const subcategoryList = subcategory.nextElementSibling;
+
+    subcategoryList.classList.toggle("ativo");
   });
 });
