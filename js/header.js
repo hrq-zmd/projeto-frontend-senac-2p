c1e9fefc5214ac249a7ea9c4b13283b27e8a95f3
Henrique Zmuda <zmuda.fis@gmail.com>
2026-08-26 21:54:32 -0300

    add comments to js codes

--- js/header.js
+++ js/header.js
@@ -1,6 +1,6 @@
-const toggleBtn = document.querySelector(".menu-toggle");
-const menu = document.querySelector(".menu");
+const toggleBtn = document.querySelector(".menu-toggle"); // criação da constante toggleBtn ("toggle button") = método querySelector aplicado ao document (HTML), usando como parâmetro a classe .menu-toggle (CSS)
+const menu = document.querySelector(".menu"); // criação da constante menu = método querySelector aplicado ao document (HTML), usando como parâmetro a classe .menu (CSS)
 
-toggleBtn.addEventListener("click", () => {
+toggleBtn.addEventListener("click", () => { //aplica o método addEventListener, composto de uma arrow function, tendo "click" como parâmetro, de modo que, ao ser chamada, a função acessa a classe e muda o seu valor para "ativo"
   menu.classList.toggle("ativo");
 });
--- js/sidebar.js
+++ js/sidebar.js
@@ -1,23 +1,26 @@
-const departmentTitles = document.querySelectorAll(".department_title");
+const departmentTitles = document.querySelectorAll(".department_title"); // criação da constante departmentTitles = aplicação do método querySelectorAll, para selecionar todos os parâmetros iguais (true) à classe .department_title (CSS)
 
-departmentTitles.forEach((title) => {
+departmentTitles.forEach((title) => { // método forEach aplicado ao vetor departmentTitles em que, através de uma arrow function, para cada parâmetro title se aplica o método addEventListener 
   title.addEventListener("click", () => {
-    const departmentList = title.nextElementSibling;
+    const departmentList = title.nextElementSibling; // ao receber o evento "click", à constante departmentList é atribuida a propriedade nextElementSibling (próximo elemento irmão)
 
-    departmentList.classList.toggle("ativo");
+    departmentList.classList.toggle("ativo"); // acessa a lista de classes de departmentList e aplica o método toggle que alterna para os estilos da classe:ativo para "ativo"
   });
 });
 
 const departmentSubcategories = document.querySelectorAll(
+  // criação da constante departmentTitles = aplicação do método querySelectorAll, para selecionar todos os parâmetros iguais (true) à classe .department_subcategory (CSS)
   ".department_subcategory",
 );
 
 departmentSubcategories.forEach((subcategory) => {
+  // método forEach aplicado ao vetor departmentTitles em que, por meio de uma arrow function, para cada parâmetro title se aplica o método addEventListener
   subcategory.addEventListener("click", (event) => {
-    event.preventDefault();
+    // ao receber o evento "click" execute a função, recebendo o objeto event do navegador ao ser executado o click
+    event.preventDefault(); // aplica o método preventDefault ao objeto evento recebido, previnindo o comportamento padrão do navegador
 
-    const subcategoryList = subcategory.nextElementSibling;
+    const subcategoryList = subcategory.nextElementSibling; // à constante departmentList é atribuida a propriedade nextElementSibling (próximo elemento irmão) de subcategory
 
-    subcategoryList.classList.toggle("ativo");
+    subcategoryList.classList.toggle("ativo"); // acessa a propriedade lista de classes de subcategoryList e aplica o método toggle que alterna para os estilos da classe:ativo
   });
 });
