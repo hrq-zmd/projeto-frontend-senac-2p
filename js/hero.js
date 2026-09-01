244fb990529a0a358d7cf8a7619e159a10d0760a
Henrique Zmuda <zmuda.fis@gmail.com>
2026-08-31 21:37:04 -0300

    fix typing errors and syntax

--- js/hero.js
+++ js/hero.js
@@ -2,29 +2,29 @@ let slideIndex = 1;
 showSlides(slideIndex);
 
 function changeSlide(n) { //Avança ou volta slides
-    showSlide(slideIndex += n);
+    showSlides(slideIndex += n);
 }
 
-function showSlides(n) {
+function showSlides(n) { // Controla a exibição e lógica da sequencia de slides
     let i;
-    let slides = document.getElementsByClassName("slide");
+    let slides = document.getElementsByClassName("slide"); // Busca todos os elementos com a classe .slide
         
-        if(n > slides.length) {
+        if(n > slides.length) { // quando acabar a apresentação, retorna ao início
             slideIndex = 1;
         }
 
-        if(n < 1) {
+        if(n < 1) { // se for antes do primeiro slide, vai para o ultimo
             slideIndex = slides.length;
         }
 
-        for(i = 0; i < slides.length; i++) {
-            slides[i].computedStyleMap.display = "none";
+        for(i = 0; i < slides.length; i++) { // Esconde todos os slides mudando o display para none
+            slides[i].style.display = "none";
         }
     
-        slides[slidesIndex - 1].computedStyleMap.display = "block";
+        slides[slideIndex - 1].style.display = "block"; // mostra apenas o slide atual mudando o display para block
 }
 
-setInterval( () => {
+setInterval( () => { // passa os slides automaticamente a cada 5 segundos (5000 ms)
     changeSlide(1);
 },
 5000);
